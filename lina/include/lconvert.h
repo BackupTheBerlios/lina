9e0a2b9fc3dac5b1ef6370d5a1ee12eba220e2f1
zwerg <zwerg>
2003-11-26 18:08:38 +0000

    fixed for libstdc++3

--- lina/include/lconvert.h
+++ lina/include/lconvert.h
@@ -28,7 +28,7 @@
 template<class T>
 inline bool LStringToDigit(T &t,
                            const std::string &s,
-                           std::ios_base & (*f)(std::ios_base&)=std::dec)
+                           std::ios & (*f)(std::ios&)=std::dec)
 {
   std::istringstream iss(s);
   return !(iss>>f>>t).fail();
