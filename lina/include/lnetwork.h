6c7be8d6f3f649cbe330eab67c17e30e8ccef9d3
tobgle <tobgle>
2004-04-19 21:51:39 +0000

    initial

---
+++ lina/include/lnetwork.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tobias Glaesser                                 *
+ *   tobi.web@gmx.de                                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef LNETWORK_H
+#define LNETWORK_H
+
+#include <netxx/tls/netxx.h>
+
+extern const Netxx::port_type lina_port;
+
+/* This is NOT final and only for testing purposes right now.*/
+
+enum LNetMsg {
+  
+ LNetConnect = 0,
+ LNetDisconnect = 1,
+
+};
+ 
+#endif //LNETWORK_H
